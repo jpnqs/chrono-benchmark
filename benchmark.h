e2ce6474711f99b01969274d893aaa33ba5b947b
Jonas <jonas-gruempel@gmx.de>
2021-02-26 11:59:56 +0100

    Update benchmark.h

--- benchmark.h
+++ benchmark.h
@@ -4,9 +4,12 @@
 namespace bny {
 
 	constexpr long double c_msfactor = 1000.0;
+	constexpr long double c_secFactor = 1000.0 * c_msfactor;
 
 	typedef std::chrono::time_point<std::chrono::steady_clock> time_point_t;
+	typedef long double µs_t;
 	typedef long double ms_t;
+	typedef long double sec_t;
 
 	/// <summary>
 	/// Class for benchmarking 
@@ -24,19 +27,32 @@ namespace bny {
 		/// and calculate the ellapsed milliseconds
 		/// </summary>
 		/// <returns>ellapsed milliseconds</returns>
-		ms_t end();
+		void end();
+
+		/// <summary>
+		/// Get the ellapsed microseconds
+		/// </summary>
+		/// <returns>ellapsed microseconds</returns>
+		µs_t get_µs();
 
 		/// <summary>
 		/// Get the ellapsed milliseconds
 		/// </summary>
 		/// <returns>ellapsed milliseconds</returns>
-		ms_t get_ellapsed_time();
+		ms_t get_ms();
+
+		/// <summary>
+		/// Get the ellapsed seconds
+		/// </summary>
+		/// <returns>ellapsed seconds</returns>
+		sec_t get_sec();
 
 	private:
 		time_point_t m_startTime;
 		time_point_t m_endTime;
-		ms_t m_ellapsedTime;
+		µs_t m_ellapsedTime;
 
 	};
 
 }
+
